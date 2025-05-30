c0142eb191fe16dc355457a44a7cadc9c0d0d546
pceal <patricia.cerezo@hotmail.com>
2025-05-30 13:06:03 +0200

    instalaviones

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+
+
+
+
+
+
+
+
+//SERVIDOR
+app.listen(PORT, ()=>{
+    console.log(`Server listening on http://localhost:${PORT}`)
+})
