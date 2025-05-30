1c20c0463b693c66b00667db9db0ed5b530b5e58
pceal <patricia.cerezo@hotmail.com>
2025-05-30 17:14:38 +0200

    ultimos cambios para clonar

---
+++ controllers/UserController.js
@@ -0,0 +1,11 @@
+const { User } = require('../models/index.js');
+const UserController = {
+    async create(req, res) {
+        try{
+            const user = await User.create(req.body)
+            res.status(201).send({ msg: 'Usuario creado con éxito', user })
+        } catch(error){
+            res.status(500).send(error)
+        }
+    }
+}
